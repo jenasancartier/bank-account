078fe5538a3be7075b0f1c55066b1b22accf54fb
Jena SanCartier and Kevin Johnston <kevin@>
2016-08-23 15:52:53 -0700

    utilize prototypes to update users balance via withdrawing and/or depositing

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
 //business logic:
-var initialInfoArray = [];
-var depositWithdrawArray = [];
 var currentBalanceArray = [];
 
 function BankAccount(name, balance) {
@@ -16,30 +14,27 @@ BankAccount.prototype.withdraw = function(withdrawAmount) {
   this.balance -= withdrawAmount;
 }
 
-
-
 //user interface logic:
 $(document).ready(function(){
   $("form#new-account").submit(function(event){
     event.preventDefault();
 
     var inputtedName = $("input#register-name").val();
-    var inputedAmount = $("input#initial-deposit").val();
-    var newBankAccount = new BankAccount(inputtedName, inputedAmount);
+    var inputtedAmount = parseInt($("input#initial-deposit").val());
+    var newBankAccount = new BankAccount(inputtedName, inputtedAmount);
     console.log(newBankAccount);
+    $("form#banking").show();
+    $("#currentBalance").text(newBankAccount.balance);
 
   $("form#banking").submit(function(event){
     event.preventDefault();
-
-    var depositAmount = $("#deposit-amount").val();
-    var withdrawAmount = $("#withdraw-amount").val();
-
-
-
-
-
-
-    })
-
-  })
+    var depositAmount = parseInt($("#deposit-amount").val());
+    var withdrawAmount = parseInt($("#withdraw-amount").val());
+    newBankAccount.withdraw(withdrawAmount);
+    newBankAccount.deposit(depositAmount);
+    console.log(newBankAccount.balance);
+    $("currentBalance").empty("");
+    $("currentBalance").text(newBankAccount.balance);
+    });
+  });
 })
