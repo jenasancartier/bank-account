a2df0c1306223c591adf99c009d0e7efcdd47363
Jena SanCartier and Kevin Johnston <kevin@>
2016-08-23 14:00:45 -0700

    fix form formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
 //business logic:
+var depositWithdrawArray = [];
+var currentBalanceArray = [];
+
+function BankAccount(name, initialDeposit) {
+  this.accountName = name;
+  this.initialDeposit = initialDeposit;
+
+}
+
+
+function DepositWithdraw(deposit, withdraw)
 
 
 //user interface logic:
